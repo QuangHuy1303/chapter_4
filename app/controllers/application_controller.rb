151495be7c7d4a10b7fcf0ca708210e1142f26cd
HuyNQ <huynq1303@gmail.com>
2018-03-23 10:16:48 +0700

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   def hello
     render html: "hello, world!"
+  end
+  def help
+  end
+  def contact
+  end
+  def about
+  end
+  def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get "foo/bar"
 
   get "static_pages/home"
-
-  get "cd static_pages/help"
+  get "users/new"
+  get "/help", to: "static_pages#help"
+  get "/contact", to: "static_pages#contact"
+  get "/about", to: "static_pages#about"
+  get "/signup", to: "users#new"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
